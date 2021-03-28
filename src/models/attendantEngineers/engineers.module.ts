3e79795d75c9e1995bb2546f48e443402b83a48e
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2021-03-28 11:35:41 +0300

    Added soft deletion for engineers entity

--- src/models/attendantEngineers/engineers.module.ts
+++ src/models/attendantEngineers/engineers.module.ts
@@ -6,11 +6,24 @@ import {
 } from './schemas/engineer.schema';
 import { EngineersService } from './engineers.service';
 import { EngineersController } from './engineers.controller';
+import { Schema } from 'mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([
-      { name: AttendantEngineer.name, schema: AttendantEngineerSchema }
+    MongooseModule.forFeatureAsync([
+      {
+        name: AttendantEngineer.name,
+        useFactory: (): Schema => {
+          const schema = AttendantEngineerSchema;
+          // eslint-disable-next-line @typescript-eslint/no-var-requires
+          schema.plugin(require('mongoose-delete'), {
+            overrideMethods: true,
+            deletedAt: true,
+            indexFields: ['deleteAt']
+          });
+          return schema;
+        }
+      }
     ])
   ],
   controllers: [EngineersController],
